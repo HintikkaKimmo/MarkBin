452b5412c478f99f9254ad69b2204d37a97777bb
HintikkaKimmo <hintikkakimmo@gmail.com>
2016-07-11 21:31:05 +0100

    added bootstrap 3.3.6 and created header

---
+++ client/components/header.js
@@ -0,0 +1,27 @@
+/**
+ * Created by Kimmo on 11.7.2016.
+ */
+
+import React, { Component } from 'react';
+
+class Header extends Component {
+    render() {
+        return (
+            <nav className="nav navbar-default">
+                <div className="navbar-header">
+                    <a className="navbar-brand">Markbin</a>
+                </div>
+                <ul className="nav navbar-nav">
+                    <li>
+                        <a>Sign up</a>
+                    </li>
+                    <li>
+                        <a>Create Bin</a>
+                    </li>
+                </ul>
+            </nav>
+        );
+    }
+}
+
+export default Header;
