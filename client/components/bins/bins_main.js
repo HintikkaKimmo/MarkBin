2c43daf05a5d5a17ba2a803e72c8d855a75c35ff
HintikkaKimmo <Hintikkakimmo@gmail.com>
2016-07-13 09:55:03 +0100

    added markdown library and created bins_viewer component

--- client/components/bins/bins_main.js
+++ client/components/bins/bins_main.js
@@ -5,13 +5,15 @@ import React, { Component } from 'react';
 import { createContainer } from 'meteor/react-meteor-data';
 import { Bins } from '../../../imports/collections/bins';
 import BinsEditor from './bins_editor'
+import BinsViewer from './bins_viewer';
 
 class BinsMain extends  Component {
     render() {
         if (!this.props.bin) { return <div>Loading...</div>; }
         return (
             <div>
-                <BinsEditor bin={this.props.bin} />
+                <BinsEditor bin={this.props.bin}/>
+                <Binsviewer bin={this.props.bin}/>
             </div>
         );
     }
---
+++ client/components/bins/bins_viewer.js
@@ -0,0 +1,18 @@
+/**
+ * Created by kimmo on 13.7.2016.
+ */
+
+import React, { Component } from 'react';
+import { markdown } from 'markdown';
+
+class BinsViewer extends Component {
+  render() {
+    return (
+      <div className="col-xs-4">
+        <h5>Output</h5>
+      </div>
+    );
+  }
+}
+
+export default BinsViewer;
